db0eb075f975f29f57bc18f9de3fb8690f21344e
sarcastic-verma <shivamthegreat.sv@gmail.com>
2021-01-27 21:20:37 +0530

    - removed paytm qr-code

--- src/components/payment-details/payment-details.component.jsx
+++ src/components/payment-details/payment-details.component.jsx
@@ -8,19 +8,9 @@ export const PaymentDetails = () => {
         <h1>Payment Info</h1>
         <br/>
         <br/>
-        <h4>For Paytm Wallet</h4>
-        <img
-            alt="PayTm Qr Code"
-            className="img-fluid rounded shadow-lg"
-            src={img}
-            style={{width: "250px"}}
-        />
-
-        <br/>
-        <br/>
         <br/>
         <div className="typography-line">
-            <h4>For Debit/Credit Card use <br/> <a
+            <h4>To pay using Debit/Credit Card,UPI use <br/> <a
                 href="https://rzp.io/l/FkX8OPWMl"><h3 className={'col'}>https://rzp.io/l/FkX8OPWMl</h3></a>
             </h4>
         </div>
